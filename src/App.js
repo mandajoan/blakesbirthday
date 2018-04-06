1a86ca13f6324badba303fdf62ca0f11e71cdb69
Amanda Dearman <adearman@scadvisoryservices.com>
2018-04-06 16:41:00 -0700

    complete

--- src/App.js
+++ src/App.js
@@ -1,19 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Surprise from './surpise'
+
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            show: false
+        }
+    }
+
+    handleClick() {
+        this.setState({
+                show:true
+            })
+            
+        }
+            
+        
+    
+    showBlake() {
+        return (
+            <Surprise />
+            )
+        
+    }
+  
+    showMessage() {
+        return (
+            <div>
+            <div>
+                <img src="../images/ninjagif.gif" />
+            </div>
+            <p className="App-intro">
+                I can't believe you are 5 years old!
         </p>
-      </div>
+            <button className="surpriseButton" onClick={(event) => { this.handleClick(event) }} > Click for a Surprise! </button>
+            </div >
+            
+            )
+    }
+    blake() {
+        if (this.state.show == true) {
+            return (
+                this.showBlake()
+                )
+        } else {
+            return (
+                this.showMessage()
+                )
+        }
+    }
+    render() {
+        let closemodal = () => this.setState({
+            show: false
+        })
+    return (
+        <div className="App">
+         
+        <header className="App-header">
+          
+                <h1 className="titleSection">Happy Birthday</h1>
+                <h1 className="titleSection">my Little Ninja!</h1>
+            </header>
+        {this.blake()} 
+       
+            </div>
+        
     );
   }
 }
---
+++ src/surpise.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Modal } from 'react-bootstrap';
+import './App.css';
+
+class Surprise extends Component {
+    render() {
+        return (
+            
+           
+           
+            <div>
+                <br />
+                            <img className="blake" src="../images/blake.jpg" />  
+                            <h2> You are the best ninja ever!</h2>
+                            <h3> Love, Taylor </h3>
+                            
+            </div >
+            
+            )
+    }
+
+
+
+}
+
+export default Surprise;
